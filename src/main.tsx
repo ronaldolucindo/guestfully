fe4a40d25e05f43137a9c6720ebf8eb415ed8204
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2024-07-12 13:53:45 -0300

    Add basic routing config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { Routes, Route, BrowserRouter, Link, Outlet } from 'react-router-dom'
 import './index.css'
+
+// TODO: Move this to its own file and add proper styling
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/create-booking">About</Link>
+          </li>
+          <li>
+            <Link to="/edit-booking/1">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+      <Outlet />
+    </div>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<p>Home component</p>} />
+          <Route
+            path="create-booking"
+            element={<p>Create booking component</p>}
+          />
+          <Route
+            path="edit-booking/:bookingId"
+            element={<p>Edit booking component</p>}
+          />
+          <Route path="*" element={<p>Not found component</p>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
