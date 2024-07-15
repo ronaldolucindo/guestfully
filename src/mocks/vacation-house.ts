f9ec4b357aea6fbaa666db42217e4549902b3a61
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2024-07-15 19:27:59 -0300

    Add property mock data

---
+++ src/mocks/vacation-house.ts
@@ -0,0 +1,48 @@
+import houseImage from '@/assets/vacation-house.jpg'
+import bedroom from '@/assets/bedroom.jpg'
+import livingroom from '@/assets/living-room.jpg'
+import kitchen from '@/assets/kitchen.jpg'
+
+const VACATION_HOUSE_MOCK = {
+  name: 'Mar Azul House',
+  subtitle: 'Beach house with an amazing view to the ocean',
+  address: '1010 Main Street, Salvador - Bahia - Brazil',
+  guests: 6,
+  bedrooms: 3,
+  bathrooms: 3,
+  amenities: [
+    'toaster',
+    'toiletries',
+    'internet',
+    'towels',
+    'tv',
+    'surveillance',
+    'washer/dryer',
+    'blender',
+    'conditioner',
+    'desk',
+    'fridge',
+    'internet',
+  ],
+  description:
+    'Welcome to Mar Azul House, your private oasis by the turquoise waters of Salvador’s coastline. Nestled amidst swaying palm trees and overlooking the pristine Barra Beach, this beachfront retreat offers an idyllic escape for travelers seeking tranquility and sun-kissed days.',
+  images: [
+    {
+      original: houseImage,
+      thumbnail: houseImage,
+    },
+    {
+      original: bedroom,
+      thumbnail: bedroom,
+    },
+    {
+      original: livingroom,
+      thumbnail: livingroom,
+    },
+    {
+      original: kitchen,
+      thumbnail: kitchen,
+    },
+  ],
+}
+export default VACATION_HOUSE_MOCK
